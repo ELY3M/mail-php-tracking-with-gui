59b3455a4749f79ddf348a93b020d3a2d4c454d9
ELY M <elymbmx@gmail.com>
2022-10-02 21:40:52 -0500

    own mods

--- link.php
+++ link.php
@@ -2,8 +2,8 @@
 <body>
 <link rel="stylesheet" href="style.css">
 
-<title> email gen  </title>
-
+<title>Elys Mail Tracker</title>
+<h1>Elys Mail Tracker</h1>
 <div class="container column">
 <h1><form action="email.php" method="get">
 Email address from the person: <input type="text" name="email"><br>
--- log.php
+++ log.php
@@ -2,16 +2,16 @@
 <link rel="stylesheet" href="style.css">
 
 <div class="container column">
-<h1> log boek </h1>
+<h1>Log Book</h1>
 
 <?php
 
-$filename = 'https://yoururlgoeshere/log.txt';
+$filename = 'yoururlgoeshereto/log.txt';
 
 $data = file($filename);
 foreach ($data as $line_num=>$line)
 {
-    echo ' <b>'.$line_num.'</b>:'.$line.'<br/>';
+    echo '<b>'.$line_num.'</b> '.$line.'<br/>';
 }
-?> 
+?>
 </html>
